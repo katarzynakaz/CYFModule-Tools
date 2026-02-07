f213342bb291cb3ab6e66498527df2a2c63e2cbc
katarzynakaz <l453r3@gmail.com>
2026-02-07 12:15:54 +0000

    branch

--- individual-shell-tools/awk/script-07-stretch.sh
+++ individual-shell-tools/awk/script-07-stretch.sh
@@ -7,4 +7,4 @@ set -euo pipefail
 # TODO: Write a command to output just the names of each player along with the total of adding all of that player's scores.
 # Your output should contain 6 lines, each with one word and one number on it.
 # The first line should be "Ahmed 15". The second line should be "Basia 37"
-awk ''{print $1, {sum +=2} END {print sum}' scores-table.txt
+awk '{print $1, {sum +=2} END {print sum}' scores-table.txt
--- individual-shell-tools/sed/script-05.sh
+++ individual-shell-tools/sed/script-05.sh
@@ -6,10 +6,19 @@ set -euo pipefail
 # If a line starts with a number and a space, make the line instead end with a space and the number.
 # So line 6 which currently reads "37 Alisha" should instead read "Alisha 37".
 # The output should contain 11 lines.
+
+# --regexp-extended for less slashes
+sed '/^[0-9]\+ / s/^\([0-9]\+\) \(.*\)/\2 \1/' input.txt
+
 sed 's|^\([0-9]*\) \(.*\)|\2 \1|' input.txt
-# s/…/…/g
-# s/ pattern / replacement /
-# [^ ] match any not a space”
+sed 's#^\([0-9][0-9]*\) \(.*\)#\2 \1#' input.txt
+
+# s/ pattern / replacement / if global
+# so
+
+# [^ ] match any not spaces
 # ^start of line
 # \([0-9]*\ all occurances at start of line
 # \2 \1 swap order
+sed -E 's/^([0-9]+) (.*)$/\2 \1/' input.txt
+# -E to avoid escape and use +
